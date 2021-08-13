c8ff60594e9974ee9d04312762a56008b9f77649
kin0ako <email@example.com>
2021-08-13 17:57:55 +0900

    FrontController作成とコメント追加

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.annotation.WebFilter;
  * Servlet Filter implementation class EncodingFilter
  */
 @WebFilter("/*")
+//文字化けしないための「おまじない」
 public class EncodingFilter implements Filter {
 
     /**
