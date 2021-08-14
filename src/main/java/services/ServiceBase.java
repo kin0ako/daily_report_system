c71b7ea0f3dcc0da8b6003db4703843bb511e5a5
kin0ako <email@example.com>
2021-08-14 22:48:16 +0900

    EmployeeValidator、EmployeeService作成

---
+++ src/main/java/services/ServiceBase.java
@@ -0,0 +1,26 @@
+package services;
+
+import javax.persistence.EntityManager;
+
+import utils.DBUtil;
+
+/**
+ * DB接続に関わる共通処理を行うクラス
+ */
+//SQL実行に共通で必要となる EntityManager インスタンスの作成やクローズ処理を実装
+public class ServiceBase {
+
+    /**
+     * EntityManagerインスタンス
+     */
+    protected EntityManager em = DBUtil.createEntityManager();
+
+    /**
+     * EntityManagerのクローズ
+     */
+    public void close() {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+}
